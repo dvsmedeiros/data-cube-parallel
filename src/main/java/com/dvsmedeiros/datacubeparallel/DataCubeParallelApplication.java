60e25494da4965d114c3cd3db8e28a7f75172700
Davisson <dvisson.medeiros@gmail.com>
2017-05-18 21:22:44 -0300

    implementa comparação de leitura do cubo com Strem e BufferedReader

--- src/main/java/com/dvsmedeiros/datacubeparallel/DataCubeParallelApplication.java
+++ src/main/java/com/dvsmedeiros/datacubeparallel/DataCubeParallelApplication.java
@@ -1,5 +1,13 @@
 package com.dvsmedeiros.datacubeparallel;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +17,38 @@ public class DataCubeParallelApplication {
 	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(DataCubeParallelApplication.class, args);
 
-		Thread.sleep(60000);
+		readWithStream("D12_T1M_S0_C1K_100.txt");
+		readWithBufferedReader("D12_T1M_S0_C1K_100.txt");
+
+	}
+
+	public static void readWithStream(String path) {
+		long start = System.currentTimeMillis();
+
+		try (Stream<String> stream = Files.lines(Paths.get(path))) {
+
+			List<String> list = stream.collect(Collectors.toList());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		long end = System.currentTimeMillis();
+		System.out.println("STREAM: " + (end - start));
+	}
+
+	public static void readWithBufferedReader(String path) {
+		long start = System.currentTimeMillis();
+
+		try (BufferedReader br = Files.newBufferedReader(Paths.get(path))) {
+
+			List<String> list = br.lines().collect(Collectors.toList());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		long end = System.currentTimeMillis();
+		System.out.println("BUFFERED READER: " + (end - start));
 	}
 }
